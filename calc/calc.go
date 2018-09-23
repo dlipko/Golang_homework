d75cd76ad1d7d6d76e0492e423d2f2408480db13
dlipko <lipko_dima@mail.ru>
2018-09-23 21:28:00 +0300

    add numbers flag

--- calc/calc.go
+++ calc/calc.go
@@ -10,69 +10,105 @@ import (
 
 func main() {
 	out := os.Stdout
-	if !(len(os.Args) == 2) {
-		panic("usage go run main.go 25+=")
+
+	var expression string
+	var isNumbers bool
+
+	if os.Args[1] == "-numbers" {
+		expression = strings.Join(os.Args[2:], " ")
+		isNumbers = true
+	} else {
+		expression = strings.Join(os.Args[1:], "")
+		isNumbers = false
 	}
 
-	expression := strings.Replace(strings.Join(os.Args[1:], ""), " ", "", -1)
-	fmt.Println(expression)
-	err := calc(out, expression)
-	if err != nil {
-		fmt.Fprintf(out, "invalid syntax")
+	fmt.Println(isNumbers, expression)
+
+	calc(out, expression, isNumbers)
+}
+
+func pop(stack *[]int) (value int) {
+	value = (*stack)[len(*stack)-1]
+	*stack = (*stack)[:len(*stack)-1]
+	return value
+}
+
+func numberCheck(stack *[]int, number *int) (int, int, *int) {
+	var y int
+	x := pop(stack)
+	if number != nil {
+		y = *number
+		number = nil
+	} else {
+		y = pop(stack)
+	}
+	return x, y, number
+}
+
+func catchWrongExpr() {
+	if err := recover(); err != nil { //catch
+		fmt.Println("Wrong expression")
 	}
 }
 
-func pop(stack *[]int) (int, []int) {
-	return (*stack)[len(*stack)-1], (*stack)[:len(*stack)-1]
-}
+func calc(out io.Writer, expression string, isNumbers bool) {
+	defer catchWrongExpr()
 
-func calc(out io.Writer, expression string) error {
 	var stack []int
+	var number *int = nil
 	for _, char := range expression {
 		var x, y int
 		switch string(char) {
 		case "+":
-			{
-				x, stack = pop(&stack)
-				y, stack = pop(&stack)
-				stack = append(stack, y+x)
+			x, y, number = numberCheck(&stack, number)
+			fmt.Println(x, " + ", y, " = ", x+y)
+			stack = append(stack, y+x)
 
-			}
 		case "-":
-			{
-				x, stack = pop(&stack)
-				y, stack = pop(&stack)
-				stack = append(stack, y-x)
+			x, y, number = numberCheck(&stack, number)
+			fmt.Println(x, " - ", y, " = ", x-y)
+			stack = append(stack, y-x)
 
-			}
 		case "*":
-			{
-				x, stack = pop(&stack)
-				y, stack = pop(&stack)
-				stack = append(stack, y*x)
-			}
+			x, y, number = numberCheck(&stack, number)
+			stack = append(stack, y*x)
+
 		case "/":
-			{
-				x, stack = pop(&stack)
-				y, stack = pop(&stack)
-				stack = append(stack, y/x)
-			}
+			x, y, number = numberCheck(&stack, number)
+			stack = append(stack, y/x)
 
 		case "=":
-			{
-				fmt.Fprintf(out, strconv.Itoa((stack[len(stack)-1])))
-			}
+			fmt.Fprintf(out, strconv.Itoa((stack[len(stack)-1])))
 
 		default:
-			{
-				number, error := strconv.Atoi(string(char))
+			numeral, error := strconv.Atoi(string(char))
+
+			if isNumbers {
+
 				if error != nil {
-					return error
+					if number != nil {
+						stack = append(stack, *number)
+					}
+					number = nil
+				} else {
+					if number == nil {
+						number = new(int)
+					}
+					(*number) *= 10
+					(*number) += numeral
 				}
-				stack = append(stack, number)
+
+			} else {
+
+				if error != nil {
+					fmt.Println("find syntax error")
+				} else {
+					stack = append(stack, numeral)
+				}
+
 			}
+
 		}
 
 	}
-	return nil
 }
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -8,10 +8,7 @@ import (
 func TestAdd(t *testing.T) {
 	out := new(bytes.Buffer)
 	expected := "5"
-	err := calc(out, "32+=")
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	calc(out, "32+=", false)
 	result := out.String()
 	if result != expected {
 		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
@@ -21,10 +18,7 @@ func TestAdd(t *testing.T) {
 func TestSub(t *testing.T) {
 	out := new(bytes.Buffer)
 	expected := "1"
-	err := calc(out, "32-=")
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	calc(out, "32-=", false)
 	result := out.String()
 	if result != expected {
 		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
@@ -34,10 +28,7 @@ func TestSub(t *testing.T) {
 func TestDiv(t *testing.T) {
 	out := new(bytes.Buffer)
 	expected := "4"
-	err := calc(out, "82/=")
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	calc(out, "82/=", false)
 	result := out.String()
 	if result != expected {
 		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
@@ -47,10 +38,7 @@ func TestDiv(t *testing.T) {
 func TestMult(t *testing.T) {
 	out := new(bytes.Buffer)
 	expected := "16"
-	err := calc(out, "82*=")
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	calc(out, "82*=", false)
 	result := out.String()
 	if result != expected {
 		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
@@ -60,10 +48,7 @@ func TestMult(t *testing.T) {
 func TestDiverseExpr(t *testing.T) {
 	out := new(bytes.Buffer)
 	expected := "6"
-	err := calc(out, "825*+132*+4-/=")
-	if err != nil {
-		t.Errorf("test for OK Failed - error")
-	}
+	calc(out, "825*+132*+4-/=", false)
 	result := out.String()
 	if result != expected {
 		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
@@ -72,8 +57,30 @@ func TestDiverseExpr(t *testing.T) {
 
 func TestWrongSyntax(t *testing.T) {
 	out := new(bytes.Buffer)
-	err := calc(out, "82(5*+132*+4-/=")
-	if err == nil {
-		t.Errorf("results not match expected error")
+	expected := "6"
+	calc(out, "82(5*+132*+4-/=", false)
+	result := out.String()
+	if result != expected {
+		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
+	}
+}
+
+func TestDiverseNumberExpr(t *testing.T) {
+	out := new(bytes.Buffer)
+	expected := "-26"
+	calc(out, "82 25 34*+11 36 2*+48-/=", true)
+	result := out.String()
+	if result != expected {
+		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
+	}
+}
+
+func TestNumbersWrongSyntax(t *testing.T) {
+	out := new(bytes.Buffer)
+	expected := "-26"
+	calc(out, "82 ^^25 (((34*)))+11$$$### 36 2*+48-/=", true)
+	result := out.String()
+	if result != expected {
+		t.Errorf("results not match\nGot: %v\nExpected: %v", result, expected)
 	}
 }
